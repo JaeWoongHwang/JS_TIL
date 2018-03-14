0f99f642661f779b8525a76865ccee2837fbccf9
JaeWoongHwang <jwngh15@gmail.com>
2018-03-14 16:57:21 +0900

    Today I learned

---
+++ Lecture10.js
@@ -0,0 +1,20 @@
+// function fuctionName(인자1, 인자2){
+//   실행할 code
+//   return result;
+// }
+
+function return_test(){
+  return;
+  console.log("실행되지 않는 코드");
+}
+
+function print(message){
+  console.log("Print function in");
+  console.log(message);
+  console.log("Print function out");
+}
+
+function sum(arg1, arg2){
+  var result = arg1 + arg2;
+  return result;
+}
---
+++ Lecture11.js
@@ -0,0 +1,11 @@
+// 관계 연산자
+// Relational Operator
+// 기본 자료형은 boolean type
+var a = 1, b = 2, c = 3, d = 4;
+var e = "a", f="b", g ="c", h = "d";
+
+// 논리 연산자
+// Logical Operator
+// AND(&&) -  두 피연사 모두 true 면 true 리턴
+// OR(||) - 두 피연산자 중 하나라도 true 면 true 리턴
+// NOT(!) - 피연산자가 true면 false리턴, false면 true 리턴
