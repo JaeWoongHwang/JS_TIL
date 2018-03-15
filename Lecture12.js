cfec095774cbce58c081d513d58532798cd4ea51
JaeWoongHwang <jwngh15@gmail.com>
2018-03-15 10:05:13 +0900

    Today I Learned

---
+++ Lecture12.js
@@ -0,0 +1,18 @@
+// 연산자 끼리의 우선순위
+// 1. Logical Operator : NOT(!)
+// 2. Arithmetic Operator : *, /, %, +, -
+// 3. Relational Operator : >,<,<=,>=,==,!=
+// 4. Logical Operator : AND(&&)
+// 5. Logical Operator : OR(||)
+
+// 우선순위 순으로 정리한 연산자
+// ++, --
+// !
+// *, /, %
+// +, -
+// <, <=, >, >=
+// ==, !=
+// &&
+// ||
+
+2 * 3 > 4 + 5 && 6 / 3 == 2 || !false
---
+++ Lecture13.js
@@ -0,0 +1,19 @@
+// String 이어 붙이기
+// 1. .concat 함수 사용 (concatenate의 약어)
+// 2. 더하기(+)연산자 사용
+
+var str  = "Hello";
+str.length;
+str["length"];
+var str2 = "World";
+str.concat(str2):
+var str3 = str.concat(str2);
+str.concat(str2).concat("!");
+"Hello".concat("World").concat("!");
+"Hello".length;
+str+str2;
+"Hello"+"World";
+
+// 문자열과 숫자를 더하면 숫자를 문자열로 변환하고 더한 결과를 출력해준다
+"PI is" + 3.14;
+3.14 + "is PI"
